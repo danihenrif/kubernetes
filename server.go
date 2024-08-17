5e55d1d4226b7c2480a768dd714b7dd34151197e
danihenrif <danielhrqs4@yahoo.com>
2024-08-16 21:39:02 -0300

    aula 98

--- server.go
+++ server.go
@@ -1,11 +1,18 @@
 package main
 
-import "net/http"
+import (
+		"net/http"
+		"os"
+		"fmt"
+	   )
 func main(){
-	http.HandleFunc("/",Hello)
+	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8000", nil)	
 }
 
 func Hello(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("<h1>Hello Full Cycle!!! </h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	//w.Write([]byte("<h1>Hello Full Cycle!!! </h1>"))
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s." , name, age)
 } 
